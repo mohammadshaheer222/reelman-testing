a28f344fa0155e1a4e34cbfd4bb54462134920f8
mohammad shaheer <shaheercse2022@gmail.com>
2024-04-12 23:01:46 +0530

    working footer

---
+++ src/Components/Home/Footer.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-white px-8 flex flex-co  gap-8 py-4 justify-center items-center flex-wrap space-x-6 w-full">
+      <h1 className="text-lg">LOGO</h1>
+      <div className="flex justify-center items-center">
+        <div className="text-balance max-w-sm ">
+          <h1 className="text-lg pb-4">About</h1>
+          <p className="font-spaceMono text-gray-400">
+            We simply let ourselves be carried away by natural emotions and by
+            the day without it or by the course of events.
+          </p>
+        </div>
+        <div className="text-center">
+          <h1 className="text-lg py-4">Explore</h1>
+          <div className="flex flex-col gap-y-1 text-gray-400">
+            <Link>Home</Link>
+            <Link>About</Link>
+            <Link>Works</Link>
+            <Link>Contact</Link>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <h1>Connect</h1>
+      </div>
+      <div className="border-t-2">
+
+      </div>
+    </div>
+  );
+};
+export default Footer;
