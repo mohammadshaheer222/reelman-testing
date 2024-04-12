a5d9e1ce57984ffc3d0f969b290567ec5819ab7e
mohammad shaheer <shaheercse2022@gmail.com>
2024-04-12 21:07:11 +0530

    resposiveness done

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,62 +1,56 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { Parallax } from "react-parallax";
-import image1 from "/src/assets/images/carousel/carousel1.jpg";
-import image2 from "/src/assets/images/carousel/carousel4.jpg";
-import image3 from "/src/assets/images/carousel/carousel5.jpg"
+import Marquee from "react-fast-marquee";
+import video1 from "/src/assets/images/video/video1.gif";
+import video2 from "/src/assets/images/video/video2.gif";
 
-const Hero = () => {
-  const carousel = [
-    { images:  image1},
-    { images:  image2},
-    { images:  image3},
+const Reels = () => {
+  const videos = [
+    {
+      video: video1,
+    },
+    {
+      video: video2,
+    },
   ];
 
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-  };
-
   return (
-    <div>
-      <Slider {...settings}>
-        {/* {carousel.map((images, index) => (
-            <div key={index}>
-              <div className="w-full h-screen md:block">
-                <img
-                    className="w-full h-full object-cover"
-                    src={images.images}
-                    alt=""
-                  />
-              </div>
+    <div className="py-6">
+      {videos.map((video, index) => (
+        <div key={index} className="relative">
+          <Parallax
+            key={index}
+            strength={600}
+            bgImage={video.video}
+            style={{
+              height: "100vh",
+              width: "100vw",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="absolute bottom-0 w-full text-center text-white">
+              <Marquee
+                gradientColor={[255, 255, 255]}
+                gradientWidth={30}
+                speed={40}
+                pauseOnHover={true}
+                style={{
+                  fontSize: "3rem",
+                  fontWeight: "bold",
+                }}
+              >
+                <h1>Wedding Day Wedding Day</h1>
+              </Marquee>
             </div>
-          ))} */}
-        {carousel.map((images, index) => (
-          <div key={index}>
-            <Parallax
-              strength={600}
-              bgImage={images.images}
-              style={{
-                minHeight: "100vh",
-                height: "auto",
-                width: "100vw",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-              }}
-            />
-          </div>
-        ))}
-      </Slider>
+          </Parallax>
+        </div>
+      ))}
     </div>
   );
 };
-export default Hero;
+
+export default Reels;
