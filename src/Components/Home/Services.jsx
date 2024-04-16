0e71b6d9ccc3164ecce97383d16f3cdf5ce21531
mohammad shaheer <shaheercse2022@gmail.com>
2024-04-16 11:25:45 +0530

    add service component

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -2,13 +2,14 @@ import Marquee from "react-fast-marquee";
 
 const Services = () => {
   return (
-    <div>
+    <div className="md:py-4">
       <Marquee speed={20} pauseOnHover={true}>
         <div className="ml-6">
-          <div className="flex gap-x-6 text-xl font-bold text-gray-500 cursor-pointer">
+          <div className="flex gap-x-6 text-xl font-medium cursor-pointer md:text-4xl md:gap-x-12">
             <h3 className="">Wedding</h3>
             <h3>Brand Concept Shoot</h3>
             <h3>Engagement Shoot</h3>
+            <h3>Corporate Events</h3>
             <h3>NewBorn Portraiture</h3>
           </div>
         </div>
